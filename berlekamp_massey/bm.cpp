b7dd39b8412cbc6cb3289cac74f41d1d44c73937
Archit Srivastava <asa466@sfu.ca>
2025-01-30 22:44:24 -0800

    worked a little on bm.cpp

--- berlekamp_massey/bm.cpp
+++ berlekamp_massey/bm.cpp
@@ -2,5 +2,12 @@
 #include <iostream>
 using namespace std;
 int main(){
-int s
+    int n=10;
+int s[n]={1,2,4,8,13,20,28,215,757,2186};
+for (int i = 0; i < n ; i++)
+{
+    cout<<s[i]<<" ";
+}
+
+return 0;
 }
